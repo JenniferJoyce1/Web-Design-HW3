2636d9c69878ba450c09cc44ae9a6c7c24026f8a
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-09-24 18:05:26 -0500

    Create attendees.php

---
+++ attendees.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util.db.php");
+require_once("model-attendees.php");
+
+$pageTitle = "Attendees";
+include "view-header.php";
+$events = selectAttendees();
+include "view-attendees.php";
+include "view-footer.php";
+?>
