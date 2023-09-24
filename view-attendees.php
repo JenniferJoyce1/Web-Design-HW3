28ab50fe4f24d6377f920905b490663fa1cc9598
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-09-24 18:14:10 -0500

    Create view-attendees.php

---
+++ view-attendees.php
@@ -0,0 +1,27 @@
+<h1>Events</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr> 
+      <th>ID</th>   
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Email</th>
+        </tr>
+    </thead>
+    <tbody>
+<?php
+while ($event = $attendees -> fetch_assoc()) {
+?>
+  <tr> 
+     <td><?php echo $attendee['Attendee_ID']; ?></td>
+     <td><?php echo $attendee['Attendee_FirstName']; ?></td>
+     <td><?php echo $attendee['Attendee_LastName']; ?></td>
+     <td><?php echo $attendee['Attendee_Email']; ?></td>
+  </tr>
+<?php      
+}
+?>
+    </tbody>
+  </table>
+</div>
