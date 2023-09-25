1a992c0ede4c2c9fe45ac7c658ac9c7aadd25a46
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-09-24 19:11:20 -0500

    Update rsvps.php

--- rsvps.php
+++ rsvps.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util.db.php");
+require_once("model-rsvps.php");
 
+$pageTitle = "RSVPs";
+include "view-header.php";
+$rsvps = selectRSVPS();
+include "view-rsvps.php";
+include "view-footer.php";
+?>
