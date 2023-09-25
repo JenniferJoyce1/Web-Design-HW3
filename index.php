099c7b4e14f97435d169567dbe0bd7c44f13047e
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-09-25 17:31:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
     <h1>Homework 3</h1>
+    <p> In this homework I created a database for an event planner. There is a table for events, and attendees. 
+        The guest table connects a plus ones to an attendee. The RSVP table connects the attendees to a specific event</p>
 <?php
 include "view-footer.php";
 ?>
