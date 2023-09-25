a59e451b0e0916ac7d5e90b1e44bb3cba31e0dd9
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-09-25 14:16:19 -0500

    Update view-attendees.php

--- view-attendees.php
+++ view-attendees.php
@@ -28,36 +28,3 @@ while ($attendee = $attendees -> fetch_assoc()) {
   </table>
 </div>
 
-
-
-
-
-<h1>Events</h1>
-<div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr> 
-      <th>ID</th>   
-      <th>Name</th>
-      <th>Date</th>
-      <th>Location</th>
-      <th>Hyperlink</th>
-        </tr>
-    </thead>
-    <tbody>
-<?php
-while ($event = $events -> fetch_assoc()) {
-?>
-  <tr> 
-     <td><?php echo $event['Event_ID']; ?></td>
-     <td><?php echo $event['Event_Name']; ?></td>
-     <td><?php echo $event['Event_Date']; ?></td>
-     <td><?php echo $event['Event_Location']; ?></td>
-     <td><a href="attendees-by-event.php?id=<?php echo $event['Event_ID']; ?>">Attendees</a></td>
-  </tr>
-<?php      
-}
-?>
-    </tbody>
-  </table>
-</div>
