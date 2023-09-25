6f0a732b7c2eb8d8909fa48ea3ad189355511baf
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-09-25 14:15:29 -0500

    Update view-attendees.php

--- view-attendees.php
+++ view-attendees.php
@@ -7,6 +7,7 @@
       <th>First Name</th>
       <th>Last Name</th>
       <th>Email</th>
+      <th>Hyperlink</th>
         </tr>
     </thead>
     <tbody>
@@ -18,6 +19,41 @@ while ($attendee = $attendees -> fetch_assoc()) {
      <td><?php echo $attendee['Attendee_FirstName']; ?></td>
      <td><?php echo $attendee['Attendee_LastName']; ?></td>
      <td><?php echo $attendee['Attendee_Email']; ?></td>
+     <td><a href="guests-per-attendee.php?id=<?php echo $attendee['Attendee_ID']; ?>">Guests</a></td>
+  </tr>
+<?php      
+}
+?>
+    </tbody>
+  </table>
+</div>
+
+
+
+
+
+<h1>Events</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr> 
+      <th>ID</th>   
+      <th>Name</th>
+      <th>Date</th>
+      <th>Location</th>
+      <th>Hyperlink</th>
+        </tr>
+    </thead>
+    <tbody>
+<?php
+while ($event = $events -> fetch_assoc()) {
+?>
+  <tr> 
+     <td><?php echo $event['Event_ID']; ?></td>
+     <td><?php echo $event['Event_Name']; ?></td>
+     <td><?php echo $event['Event_Date']; ?></td>
+     <td><?php echo $event['Event_Location']; ?></td>
+     <td><a href="attendees-by-event.php?id=<?php echo $event['Event_ID']; ?>">Attendees</a></td>
   </tr>
 <?php      
 }
