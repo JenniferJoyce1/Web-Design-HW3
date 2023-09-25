5845f63ea79436a5fed8a663cce1681de51d4dbc
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-09-24 19:44:25 -0500

    Create view-guests.php

---
+++ view-guests.php
@@ -0,0 +1,27 @@
+<h1>Attendees</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr> 
+      <th>ID</th>   
+      <th>Attendee ID</th>
+      <th>Guest Name</th>
+      <th>Relationship</th>
+        </tr>
+    </thead>
+    <tbody>
+<?php
+while ($guest = $guests -> fetch_assoc()) {
+?>
+  <tr> 
+     <td><?php echo $guest['Guest_ID']; ?></td>
+     <td><?php echo $guest['Attendee_ID']; ?></td>
+     <td><?php echo $guest['Guest_Name']; ?></td>
+     <td><?php echo $guest['Guest_Relationship']; ?></td>
+  </tr>
+<?php      
+}
+?>
+    </tbody>
+  </table>
+</div>
