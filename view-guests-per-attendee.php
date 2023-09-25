b29a10641952644231e9c1db361b3d113d5c100c
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-09-25 14:19:19 -0500

    Update view-guests-per-attendee.php

--- view-guests-per-attendee.php
+++ view-guests-per-attendee.php
@@ -5,7 +5,6 @@
       <tr> 
       <th>Attendee First Name</th>   
       <th>Attendee Last Name</th>
-      <th>is bringing their </th>
       <th>Relationship</th>
       <th>Guest Name</th>
         </tr>
@@ -17,7 +16,7 @@ while ($guest = $guests -> fetch_assoc()) {
   <tr> 
      <td><?php echo $guest['Attendee_FirstName']; ?></td>
      <td><?php echo $guest['Attendee_LastName']; ?></td>
-     <td> is bringing their </td>
+    
      <td><?php echo $guest['Guest_Relationship']; ?></td>
      <td><?php echo $guest['Guest_Name']; ?></td>
   </tr>
