67cadee65eb50d7b826394d604c2fa2e1a8eae6b
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-09-24 19:46:33 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -31,6 +31,9 @@
           <li class="nav-item">
           <a class="nav-link" href="rsvps.php">RSVPS</a>
         </li>
+          <li class="nav-item">
+          <a class="nav-link" href="guests.php">Guests</a>
+        </li>
       </ul>
     </div>
   </div>
