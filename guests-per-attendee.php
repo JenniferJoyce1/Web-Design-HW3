c8fcb4d88a272e40073f5b0391ae104004bf4977
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-09-25 14:03:13 -0500

    Update and rename guests-per-attendees.php to guests-per-attendee.php

--- guests-per-attendee.php
+++ guests-per-attendee.php
@@ -1,10 +1,10 @@
 <?php
 require_once("util.db.php");
-require_once("model-guests-per-attendees.php");
+require_once("model-guests-per-attendee.php");
 
 $pageTitle = "Attendees' Guests";
 include "view-header.php";
 $attendees = selectAttendeesByEvent($_GET['id']);
-include "view-guests-per-attendees.php";
+include "view-guests-per-attendee.php";
 include "view-footer.php";
 ?>
