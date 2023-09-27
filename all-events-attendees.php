cb0a8fc1f732d4784cffb6a868ddfed048843082
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-09-26 22:20:27 -0500

    Create all-events-attendees.php

---
+++ all-events-attendees.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util.db.php");
+require_once("model-all-events-attendees.php");
+
+$pageTitle = "Events with their respective attendees";
+include "view-header.php";
+$events = selectEvents();
+include "view-all-events-attendees.php";
+include "view-footer.php";
+?>
