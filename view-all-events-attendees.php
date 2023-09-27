0d4fb35b5af498ac1b16173b31bab7c40efeb59d
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-09-26 22:38:24 -0500

    Update view-all-events-attendees.php

--- view-all-events-attendees.php
+++ view-all-events-attendees.php
@@ -7,13 +7,16 @@ while ($event = $events -> fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $event['Event_Name']; ?></h5>
       <p class="card-text"> 
+      <ul class="list-group">  
 <?php 
   $attendees = selectAttendeesByEvent($event['Event_ID']);
   while ($attendee = $attendees->fetch_assoc()) {
 ?>
+     <li class="list-group-item"><?php echo $attendee['Attendee_FirstName']; ?> - <?php echo $attendee['Attendee_LastName']; ?> - <?php echo $attendee['RSVP_Status']; ?> - <?php echo $attendee['Attendee_Email']; ?></li>
 <?php    
     }
 ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Date: <td><?php echo $event['Event_Date']; ?>Last updated 3 mins ago</small></p>
     </div>
