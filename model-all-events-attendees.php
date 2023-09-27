dd13dfa789fd2666346e91d97d3e6f0596c40588
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-09-26 22:23:12 -0500

    Update model-all-events-attendees.php

--- model-all-events-attendees.php
+++ model-all-events-attendees.php
@@ -12,4 +12,21 @@ function selectEvents() {
         throw $e;
     }
 }
+
+function selectAttendeesByEvent($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT a.Attendee_ID, a.Attendee_FirstName, a.Attendee_LastName, a.Attendee_Email,r.RSVP_Status 
+        FROM `Attendee` a JOIN RSVP r on a.Attendee_ID = r.Attendee_ID 
+        WHERE r.Event_ID=?");
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
