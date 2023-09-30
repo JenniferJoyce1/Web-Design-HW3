2d7ee3cd095f5b9bf6a22e8982458b87a6cebdcf
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-09-30 15:46:25 -0500

    Update model-guests-per-attendee.php

--- model-guests-per-attendee.php
+++ model-guests-per-attendee.php
@@ -1,11 +1,11 @@
 <?php
-function selectGuestsPerAttendee($cid) {
+function selectGuestsPerAttendee($iid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT a.Attendee_FirstName, a.Attendee_LastName, g.Guest_Relationship, g.Guest_Name 
         FROM Attendee a JOIN Guest g ON a.Attendee_ID = g.Attendee_ID
         WHERE a.Attendee_ID=?");
-        $stmt->bind_param("i", $cid);
+        $stmt->bind_param("i", $iid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
