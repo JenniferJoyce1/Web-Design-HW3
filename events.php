41cd468a11bfd61d1410755fadb39586c61c42ef
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-10 18:15:48 -0500

    Update events.php

--- events.php
+++ events.php
@@ -9,14 +9,14 @@ if (isset($_POST['actionType'])){
   switch ($_POST['actionType']) {
     case "Add":
       if (insertEvent($_POST['eName'], $_POST['eDate'], $_POST['eLocation'])) {
-        echo '<div class="alert alert-success" role="alert" > Success! Event Added.</div>"';
+        echo '<div class="alert alert-success" role="alert" > Success! Event Added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
       }
         break;
         case "Delete":
       if (deletEvent($_POST['eID'])) {
-        echo '<div class="alert alert-success" role="alert" > Success! Event Deleted.</div>"';
+        echo '<div class="alert alert-success" role="alert" > Success! Event Deleted.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
       }
