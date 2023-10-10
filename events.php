7eef930cf4e3b779e3a371594f0053a816ce8f4d
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-10 18:07:26 -0500

    Update events.php

--- events.php
+++ events.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
   switch ($_POST['actionType']) {
     case "Add":
-      insertEvent($_POST['eName'], $_POST['eDate'], $_POST['eLocation']);
+      if (insertEvent($_POST['eName'], $_POST['eDate'], $_POST['eLocation'])) {
+        echo '<div class="alert alert-success" role="alert" > Success! Event Added.</div>"';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error.</div>';
+      }
         break;
   }
 }
