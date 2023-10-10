0196e56335f350123380c0744dce93b6f7b28f41
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-10 16:21:05 -0500

    Update events.php

--- events.php
+++ events.php
@@ -4,6 +4,14 @@ require_once("model-events.php");
 
 $pageTitle = "Events";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertEvent($_POST['eName'], $_POST['eDate'], $_POST['eLocation']); 
+  }
+}
+
 $events = selectEvents();
 include "view-events.php";
 include "view-footer.php";
