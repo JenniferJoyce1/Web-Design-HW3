a938bd7703b202f169e28ff412cc6ecd299b3b98
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-11 14:27:20 -0500

    Update model-events.php

--- model-events.php
+++ model-events.php
@@ -17,7 +17,7 @@ function insertEvents($eName, $eDate, $eLocation) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("INSERT INTO 'Event' ('Event_Name', 'Event_Date', 'Event_Location') VALUES (?, ?, ?)");
-        $stmt -> bind_param("sss", $eName, $eDate, $eLocation );
+        $stmt->bind_param("sss", $eName, $eDate, $eLocation);
         $success = $stmt->execute();
         $conn->close();
         return $success;
@@ -32,7 +32,7 @@ function updateEvents($eName, $eDate, $eLocation, $eID) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("UPDATE 'Event' SET 'Event_Name' = ?, 'Event_Date' = ?, 'Event_Location' = ?) WHERE Event_ID = ?");
-        $stmt -> bind_param("sssi", $eName, $eDate, $eLocation, $eID);
+        $stmt->bind_param("sssi", $eName, $eDate, $eLocation, $eID);
         $success = $stmt->execute();
         $conn->close();
         return $success;
@@ -46,7 +46,7 @@ function deleteEvents($eID) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("DELETE FROM 'event' WHERE 'Event_ID' = ? ");
-        $stmt -> bind_param("i", $eID);
+        $stmt->bind_param("i", $eID);
         $success = $stmt->execute();
         $conn->close();
         return $success;
