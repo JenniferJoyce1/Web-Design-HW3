d8af2290f81ef2dcc3f817edd036fa881b10aef6
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-18 21:13:59 -0500

    Update view-all-events-attendees.php

--- view-all-events-attendees.php
+++ view-all-events-attendees.php
@@ -1,6 +1,6 @@
 <div class = "row">
   <div class = "col">
-    <h1>Events and their Attendees</h1>
+    <h2>Events and their Attendees</h2>
   </div>
   <div class = "col-auto">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-database-add" viewBox="0 0 16 16">
