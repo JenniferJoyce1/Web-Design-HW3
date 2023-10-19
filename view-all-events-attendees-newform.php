567c8844ac0e0f787bdaad8d00b739396ba15281
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-18 22:58:12 -0500

    Update view-all-events-attendees-newform.php

--- view-all-events-attendees-newform.php
+++ view-all-events-attendees-newform.php
@@ -29,7 +29,7 @@
             <div class="mb-3">
               <label for="aName" class="form-label">Attendee Name</label>
                          <?php
-                        eventList = selectAttendeesForInput();
+                        attendeeList = selectAttendeesForInput();
                         include "view-attendee-input-list.php"
                         ?>
             </div>
