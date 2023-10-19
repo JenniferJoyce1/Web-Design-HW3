4abc9d52770658b0ebb8aa4c73f8b52c19963adb
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-18 21:18:49 -0500

    Update model-all-events-attendees.php

--- model-all-events-attendees.php
+++ model-all-events-attendees.php
@@ -29,4 +29,36 @@ function selectAttendeesByEvent($iid) {
         throw $e;
     }
 }
+
+
+
+function updateEvents($eName, $eDate, $eLocation, $eID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE Event SET Event_Name = ?, Event_Date = ?, Event_Location = ? WHERE Event_ID = ?");
+        $stmt->bind_param("sssi", $eName, $eDate, $eLocation, $eID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteEvents($eID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM Event WHERE Event_ID = ? ");
+        $stmt->bind_param("i", $eID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
 ?>
