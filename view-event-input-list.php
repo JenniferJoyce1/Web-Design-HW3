b258a53338d0877234742c0194e799368b2eec5f
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-23 11:48:09 -0500

    Update view-event-input-list.php

--- view-event-input-list.php
+++ view-event-input-list.php
@@ -1,12 +1,10 @@
 <select class="form-select" id="eName" name="eName">
 <?php
-while ($eventItem = $eventList-> fetch_assoc())
+while ($eventItem = $eventList->fetch_assoc())
 {
 ?>
+  <option value="<?php echo $eventItem['Event_ID']; ?>"><?php echo $eventItem['Event_Name']; ?></option>
 <?php
-  <option value="<?php echo $eventItem['Event_ID']; ?> "><?php echo $eventItem['Event_Name']; ?></option>
-?>
 }
-
-  
+?>
 </select>
