5b8c41e7585b50e222d59c8c24ec8fe25afcfdb9
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-23 16:21:47 -0500

    Update guests.php

--- guests.php
+++ guests.php
@@ -4,6 +4,41 @@ require_once("model-guests.php");
 
 $pageTitle = "Guests";
 include "view-header.php";
+
+
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertEvents($_POST['gName'], $_POST['gRelationship'])) {
+        echo '<div class="alert alert-success" role="alert" > Success! Event Added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error.</div>';
+      }
+        break;
+        case "Delete":
+      if (deleteEvents($_POST['gID'])) {
+        echo '<div class="alert alert-success" role="alert" > Success! Event Deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error.</div>';
+      }
+        break;
+        case "Edit":
+      if (updateEvents($_POST['gName'], $_POST['gRelationship'],$_POST['gID'])) {
+        echo '<div class="alert alert-success" role="alert" > Success! Event Edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error.</div>';
+      }
+        break;
+  }
+}
+
+
+
+
+
+
+
 $guests = selectGuests();
 include "view-guests.php";
 include "view-footer.php";
