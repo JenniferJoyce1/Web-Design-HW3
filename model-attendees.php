699c0b00d1d010d66ab49854d51bec5879f1aa70
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-23 14:10:09 -0500

    Update model-attendees.php

--- model-attendees.php
+++ model-attendees.php
@@ -12,4 +12,49 @@ function selectAttendees() {
         throw $e;
     }
 }
+
+
+function insertEvents($aFName, $aLName, $aEmail) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO Attendee (Attendee_FirstName, Attendee_LastName, Attendee_Email) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $aFName, $aLName, $aEmail);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function updateEvents($aFName, $aLName, $aEmail, $aID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE Attendee SET Attendee_FirstName = ?, Attendee_LastName = ?, Attendee_Email = ? WHERE Attendee_ID = ?");
+        $stmt->bind_param("sssi",$aFName, $aLName, $aEmail, $aID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteEvents($aID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM Attendee WHERE Attendee_ID = ? ");
+        $stmt->bind_param("i", $aID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
