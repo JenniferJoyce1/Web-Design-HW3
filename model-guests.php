9070d854e1ebf4422da7c341179bb9fde8309b65
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-23 14:53:51 -0500

    Update model-guests.php

--- model-guests.php
+++ model-guests.php
@@ -12,4 +12,22 @@ function selectGuests() {
         throw $e;
     }
 }
+
+
+
+function selectGuestsForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT Guest_ID , Guest_Name FROM Guest ORDER BY Guest_Name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
 ?>
