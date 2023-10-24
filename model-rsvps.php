85efac307ffbc6358fb95ca91d493efb12ef6d34
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-24 10:38:30 -0500

    Update model-rsvps.php

--- model-rsvps.php
+++ model-rsvps.php
@@ -12,4 +12,53 @@ function selectRSVPS() {
         throw $e;
     }
 }
+
+
+function insertRSVPS($eID, $aID, $rStatus) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO RSVP (Event_ID, Attendee_ID, RSVP_Status) VALUES (?, ?, ?)");
+        $stmt->bind_param("iis", $eID, $aID, $rStatus);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function updateRSVPS($eID, $aID, $rStatus, $rID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE RSVP SET Event_ID = ?, Attendee_ID = ?, RSVP_Status = ? WHERE RSVP_ID = ?");
+        $stmt->bind_param("iisi", $eID, $aID, $rStatus, $rID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteRSVPS($rID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM RSVP WHERE RSVP_ID = ? ");
+        $stmt->bind_param("i", $rID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+
+
+
 ?>
