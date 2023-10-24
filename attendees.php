167fadf79595ad5d785dc045580a03fd8e5b68d0
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-23 19:53:09 -0500

    Update attendees.php

--- attendees.php
+++ attendees.php
@@ -9,21 +9,21 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertEvents($_POST['aFName'], $_POST['aLName'], $_POST['aEmail'])) {
+      if (insertAttendees($_POST['aFName'], $_POST['aLName'], $_POST['aEmail'])) {
         echo '<div class="alert alert-success" role="alert" > Success! Attendee Added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
       }
         break;
         case "Delete":
-      if (deleteEvents($_POST['aID'])) {
+      if (deleteAttendees($_POST['aID'])) {
         echo '<div class="alert alert-success" role="alert" > Success! Attendee Deleted.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
       }
         break;
         case "Edit":
-      if (updateEvents($_POST['aFName'], $_POST['aLName'], $_POST['aEmail'], $_POST['aID'])) {
+      if (updateAttendees($_POST['aFName'], $_POST['aLName'], $_POST['aEmail'], $_POST['aID'])) {
         echo '<div class="alert alert-success" role="alert" > Success! Attendee Edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
