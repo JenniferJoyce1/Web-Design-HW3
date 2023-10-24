53288a1faf8510303ea052b7d9237e83ab1bc0fc
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-23 19:52:25 -0500

    Update guests.php

--- guests.php
+++ guests.php
@@ -10,21 +10,21 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertEvents($_POST['gName'], $_POST['gRelationship'])) {
+      if (insertGuests($_POST['gName'], $_POST['gRelationship'])) {
         echo '<div class="alert alert-success" role="alert" > Success! Event Added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
       }
         break;
         case "Delete":
-      if (deleteEvents($_POST['gID'])) {
+      if (deleteGuests($_POST['gID'])) {
         echo '<div class="alert alert-success" role="alert" > Success! Event Deleted.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
       }
         break;
         case "Edit":
-      if (updateEvents($_POST['gName'], $_POST['gRelationship'],$_POST['gID'])) {
+      if (updateGuests($_POST['aID'], $_POST['gName'], $_POST['gRelationship'],$_POST['gID'])) {
         echo '<div class="alert alert-success" role="alert" > Success! Event Edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error.</div>';
