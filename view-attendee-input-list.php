d3fcb619a5707490ff7ffde5e3211716ab9758b4
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-23 19:35:33 -0500

    Update view-attendee-input-list.php

--- view-attendee-input-list.php
+++ view-attendee-input-list.php
@@ -1,10 +1,10 @@
-<select class="form-select" id = "iid" name="iid">
+<select class="form-select" id="iid" name="iid">
 <?php
-while ($attendeeItem = $attendeeList-> fetch_assoc())
+while ($attendeeItem = $attendeeList->fetch_assoc())
 {
 ?>
+  <option value="<?php echo $attendeeItem['Attendee_ID']; ?>"><?php echo $attendeeItem['Attendee_FirstName']; ?></option>
 <?php
-  <option value="<?php echo $attendeeItem['Attendee_ID']; ?> "><?php echo $attendeeItem['Attendee_FirstName']; ?></option>
-?>
 }
+?>
 </select>
