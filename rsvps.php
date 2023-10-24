8278dc7a5eb75ad541b617ebf5373bf07e365390
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-24 10:33:24 -0500

    Update rsvps.php

--- rsvps.php
+++ rsvps.php
@@ -4,6 +4,37 @@ require_once("model-rsvps.php");
 
 $pageTitle = "RSVPs";
 include "view-header.php";
+
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertRSVPS($_POST['eID'], $_POST['aID'], $_POST['rStatus'])) {
+        echo '<div class="alert alert-success" role="alert" > Success! RSVP Added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error.</div>';
+      }
+        break;
+        case "Delete":
+      if (deleteRSVPS($_POST['rID'])) {
+        echo '<div class="alert alert-success" role="alert" > Success! RSVP Deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error.</div>';
+      }
+        break;
+        case "Edit":
+      if (updateRSVPS($_POST['eID'], $_POST['aID'], $_POST['rStatus'], $_POST['rID'])) {
+        echo '<div class="alert alert-success" role="alert" > Success! RSVP Edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error.</div>';
+      }
+        break;
+  }
+}
+
+
+
+
 $rsvps = selectRSVPS();
 include "view-rsvps.php";
 include "view-footer.php";
