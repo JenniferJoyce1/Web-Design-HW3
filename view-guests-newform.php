07b40f86049c96e723b543a5c20902446197f098
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-25 14:22:12 -0500

    Update view-guests-newform.php

--- view-guests-newform.php
+++ view-guests-newform.php
@@ -16,7 +16,7 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-
+        <form>
            <div class="mb-3">
               <label for="aID" class="form-label">Attendee ID</label>
               <input type="text" class="form-control" id="aID" name="aID" value="">
