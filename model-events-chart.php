95cf0bc1032081069ac0af90ffb860478ec80c34
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-11-11 10:46:58 -0600

    Update model-events-chart.php

--- model-events-chart.php
+++ model-events-chart.php
@@ -2,7 +2,8 @@
 function selectEvents() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare(SELECT e.Event_Name, count(r.RSVP_Status) AS num_attendees FROM `Event` e JOIN `RSVP` r ON e.Event_ID=r.Event_ID WHERE r.RSVP_Status = 'Yes' GROUP BY e.Event_Name;");
+        $stmt = $conn->prepare(SELECT Event_Name, count(r.RSVP_Status) AS num_attendees FROM `Event` e JOIN `RSVP` r ON e.Event_ID=r.Event_ID 
+            WHERE r.RSVP_Status = 'Yes' GROUP BY Event_Name;");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
