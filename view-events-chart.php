517034c85375d4a2fceb2d881659619f4bd68f72
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-11-11 10:45:27 -0600

    Update view-events-chart.php

--- view-events-chart.php
+++ view-events-chart.php
@@ -1,6 +1,40 @@
 <h1>Events Chart</h1>
 
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'pie',
+    data: {
+    datasets: [{
+        data: [
+          
 <?php
 while ($event = $events -> fetch_assoc()) {    
+  echo $event['num_attendees'] . ", ";
 }
 ?>
+
+        
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+<?php
+$events = selectEvents();
+while ($event = $events -> fetch_assoc()) {    
+  echo "'" . $event['Event_Name'] . "', ";
+}
+?>
+
+    ]
+};
+  });
+</script>
