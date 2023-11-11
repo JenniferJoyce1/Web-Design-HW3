4f3aec384be0090aacadd2366812c94982b63859
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-11-11 10:04:54 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-    <h1>Homework 4</h1>
+    <h1>Homework 6</h1>
     <p> In this homework I created a database for an event planner. There is a table for events, and attendees. 
-        The guest table connects a plus ones to an attendee. The RSVP table connects the attendees to a specific event</p>
+        The guest table connects a plus ones to an attendee. The RSVP table connects the attendees to a specific event. 
+        Specifically for HW6 I added a chart tab</p>
 <?php
 include "view-footer.php";
 ?>
