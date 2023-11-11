47f24a40bc9170776dacc44ae7dba80b22da0237
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-11-11 10:23:29 -0600

    Create model-events-chart.php

---
+++ model-events-chart.php
@@ -0,0 +1,20 @@
+<?php
+function selectEvents() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare(SELECT e.Event_Name, count(r.RSVP_Status) AS num_attendees FROM `Event` e JOIN `RSVP` r ON e.Event_ID=r.Event_ID WHERE r.RSVP_Status = 'Yes' GROUP BY e.Event_Name;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+
+
+
+?>
