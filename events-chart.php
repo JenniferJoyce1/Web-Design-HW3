185b85a0a67474af2fc8650610c695a7723000f6
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-11-11 10:13:13 -0600

    Update events-chart.php

--- events-chart.php
+++ events-chart.php
@@ -1,1 +1,13 @@
 
+<?php
+require_once("util.db.php");
+require_once("model-events-chart.php");
+
+$pageTitle = "Events Chart";
+include "view-header.php";
+
+
+$events = selectEvents();
+include "view-events-chart.php";
+include "view-footer.php";
+?>
