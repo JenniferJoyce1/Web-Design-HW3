040b53e0153bdbd4d8e7c72235fe7ad9d3a06804
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-11-13 14:04:33 -0600

    Update view-events-chart.php

--- view-events-chart.php
+++ view-events-chart.php
@@ -42,23 +42,4 @@ while ($event = $events -> fetch_assoc()) {
 
 
 
-<script>
-import Component, { hbs, tracked } from '@glimmerx/component';
-import { on, action } from '@glimmerx/modifier';
 
-export default class HelloWorld extends Component {
-  @tracked count = 1;
-
-  @action increment() {
-    this.count++;
-  }
-
-  static template = hbs`
-    <h1>Welcome to the GlimmerX Playground!</h1>
-
-    <p>You have clicked the button {{this.count}} times.</p>
-
-    <button {{on "click" this.increment}}>Click</button>
-  `;
-}
-</script>
