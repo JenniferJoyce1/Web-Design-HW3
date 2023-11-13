1aae01ddd556e2a0c7a31a07d3bfe91996c8f059
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-11-13 17:06:23 -0600

    Update js-library-one.php

--- js-library-one.php
+++ js-library-one.php
@@ -35,3 +35,21 @@ export default class HelloWorld extends Component {
 }
 
 </script>
+
+
+<script>
+var granimInstance = new Granim({
+    element: '#canvas-basic',
+    direction: 'left-right',
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#ff9966', '#ff5e62'],
+                ['#00F260', '#0575E6'],
+                ['#e1eec3', '#f05053']
+            ]
+        }
+    }
+});
+  </script>
