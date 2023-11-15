ce797daf16d6558e6b0578c8a5e5be56f1929a02
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-11-15 11:10:31 -0600

    Update js-library-one.php

--- js-library-one.php
+++ js-library-one.php
@@ -75,3 +75,21 @@ w3.slideshow(".nature", 1500);
   </video>
 </div>
 
+<h2>Tools.js</h2>
+<html>
+  <head>
+    <title>Referencing HTML Elements</title>
+    <script src="dom-tools.js"></script>
+    <script>
+      function addFavoriteAnimalList() {
+        const ulElement = createUlElement(['lions', 'tigers', 'bears']);
+        const animalContainer = document.getElementById('animal-container');
+        animalContainer.appendChild(ulElement);
+      }
+    </script>
+  </head>
+  <body onload="addFavoriteAnimalList();">
+    <p>These are my favorite animals:</p>
+    <div id="animal-container"></div>
+  </body>
+</html>  
