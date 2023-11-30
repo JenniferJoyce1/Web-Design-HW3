1ab2727bcc6e5d90530d80efc805ac749d6f7f7e
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-11-30 12:48:58 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -14,7 +14,7 @@
    
     <div class="container">
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div  style="color: lightpink;"  class="container-fluid">
+  <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -25,7 +25,7 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="events.php">Events</a>
+          <a style="color: lightpink;" class="nav-link" href="events.php">Events</a>
         </li>
           <li class="nav-item">
           <a class="nav-link" href="attendees.php">Attendees</a>
