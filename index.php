231389f61ee1ab46eb816322553c3d80bb3eade1
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-11-30 12:39:25 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
     <h1><b>Perfectly Planned</b></h1>
-    <h2><em>Turning Dreams into Unforgettable Moments: Perfectly Planned, Your Event, Your Way!</em></h2>
+    <h2><em><b>Turning Dreams into Unforgettable Moments: Perfectly Planned, Your Event, Your Way!</b></em></h2>
     <p> At Perfectly Planned, we turn your dreams into reality with expert event planning. 
         Whether it's an intimate birthday, a grand wedding, or a corporate affair, our dedicated team ensures a seamless and memorable experience. 
         From concept to execution, trust Perfectly Planned to handle the details while you focus on making cherished memories</p>
