69110b3cab9da1e63dbde9f836ef187ec8839fec
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-12-02 12:14:25 -0600

    Create planner.php

---
+++ planner.php
@@ -0,0 +1,42 @@
+<?php
+require_once("util.db.php");
+require_once("model-planner.php");
+
+$pageTitle = "Planners";
+include "view-header.php";
+
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertPlanners($_POST['eID'], $_POST['pName'], $_POST['pNumber'], $_POST['pEmail'], $_POST['pSpeciality'])) {
+        echo '<div class="alert alert-success" role="alert" > Success! Planner Added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error.</div>';
+      }
+        break;
+        case "Delete":
+      if (deletePlanners($_POST['pID'])) {
+        echo '<div class="alert alert-success" role="alert" > Success! Planner Deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error.</div>';
+      }
+        break;
+        case "Edit":
+      if (updatePlanners($_POST['eID'], $_POST['pName'], $_POST['pNumber'], $_POST['pEmail'], $_POST['pSpeciality'], $_POST['pID'])) {
+        echo '<div class="alert alert-success" role="alert" > Success! Planner Edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error.</div>';
+      }
+        break;
+  }
+}
+
+
+
+
+
+$planners = selectPlanners();
+include "view-planners.php";
+include "view-footer.php";
+?>
