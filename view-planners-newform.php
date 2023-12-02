fb26a6fda48788987ae0a08a06d199d51d4cb306
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-12-02 13:12:48 -0600

    Create view-planners-newform.php

---
+++ view-planners-newform.php
@@ -0,0 +1,49 @@
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newPlannersModal">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-plus" viewBox="0 0 16 16">
+          <path d="M8.5 6a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V10a.5.5 0 0 0 1 0V8.5H10a.5.5 0 0 0 0-1H8.5V6z"/>
+          <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/>
+        </svg>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="newPlannersModal" tabindex="-1" aria-labelledby="newPlannersModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="newPlannersModalLabel">New Planner</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+         <form method="post" action="">
+           <div class="mb-3">
+              <label for="eID" class="form-label">Event ID</label>
+              <input type="text" class="form-control" id="eID" name="eID" value="">
+            </div>
+
+           <div class="mb-3">
+              <label for="pName" class="form-label">Planner Name (First and Last)</label>
+              <input type="text" class="form-control" id="pName" name="pName" value="">
+            </div>
+           <div class="mb-3">
+              <label for="pNumber" class="form-label">Planner Phone Number</label>
+              <input type="text" class="form-control" id="pNumber" name="pNumber" value="">
+            </div>
+           <div class="mb-3">
+              <label for="pEmail" class="form-label">Planner Email</label>
+              <input type="text" class="form-control" id="pEmail" name="pEmail" value="">
+            </div>
+             <div class="mb-3">
+              <label for="pSpeciality" class="form-label">Planner Speciality</label>
+              <input type="text" class="form-control" id="pSpeciality" name="pSpeciality" value="">
+            </div>
+
+                   <input type= "hidden" name="actionType" value="Add">
+            <button type="submit" class="btn btn-primary">Add Planner</button>
+        </form>
+   
+      </div>
+    </div>
+  </div>
+</div>
