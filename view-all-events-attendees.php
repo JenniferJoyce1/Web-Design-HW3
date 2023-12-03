01d223b185def3cbf037c7f2f1533348b3b234c9
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-12-02 20:23:44 -0600

    Update view-all-events-attendees.php

--- view-all-events-attendees.php
+++ view-all-events-attendees.php
@@ -27,9 +27,9 @@ while ($event = $events -> fetch_assoc()) {
 ?>
       </ul>
       </p>
-      <p class="card-text"><small class="text-body-secondary">Date: <td><?php echo $event['Event_Date']; ?> 
-        Location: <td><?php echo $event['Event_Location']; ?> 
-        Planner: <td><?php echo $planner['Planner_Name']; ?></small></p>
+      <p class="card-text"><small class="text-body-secondary">Date: <td><?php echo $event['Event_Date']; ?> </small></p>
+      <p class="card-text"><small class="text-body-secondary">Location: <td><?php echo $event['Event_Location']; ?> </small></p>
+      <p class="card-text"><small class="text-body-secondary">Planner Name: <td><?php echo $planner['Planner_Name']; ?> </small></p>
     </div>
   </div>
 <?php      
